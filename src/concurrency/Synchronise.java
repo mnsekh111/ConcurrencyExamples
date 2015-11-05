4e224936328ee264aa85e925e9af611120ee9d3d
mnsekh111 <mnsekh111@gmail.com>
2015-11-04 23:58:05 -0500

    Added Synchronized functions

---
+++ src/concurrency/Synchronise.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concurrency;
+
+class SyncCounter {
+
+    private int c = 0;
+
+    public synchronized void increment() {
+        c++;
+    }
+
+    public synchronized void decrement() {
+        c--;
+    }
+
+    public synchronized int value() {
+        return c;
+    }
+}
+
+/**
+ *
+ * @author mnsekh111
+ */
+public class Synchronise {
+
+    public static void main(String[] args) {
+
+        SyncCounter sc = new SyncCounter();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                sc.increment();
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    sc.increment();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        
+        System.out.println("Value of Counter = " + sc.value());
+    }
+}
